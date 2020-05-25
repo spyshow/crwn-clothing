c9f5efb16171b50a39f00c814d38576504a2dca5
jihad khorfan <spyshow86@gmail.com>
2020-05-25 12:05:28 +0300

    init commit

--- src/App.js
+++ src/App.js
@@ -1,26 +1,20 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import HomePage from "../src/components/homepage/homepage.component";
+import "./App.css";
+
+class App extends Component {
+  constructor() {
+    super();
+  }
+
+  render() {
+    return (
+      <div>
+        <HomePage />
+      </div>
+    );
+  }
 }
 
 export default App;
---
+++ src/components/search-box/search-box.component.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+import "./search-box.styles.css";
+
+export const SearchBox = ({ placeholder, changeHandler }) => (
+  <input
+    className="search"
+    type="search"
+    placeholder={placeholder}
+    onChange={changeHandler}
+  />
+);
