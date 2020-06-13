7ab488e2e3ebe8fb60f562f5fd2037e23dc86e0f
jihad khorfan <spyshow86@gmail.com>
2020-06-14 01:18:35 +0300

    addin a signin component

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import { Route, Switch } from "react-router-dom";
 import Header from "./components/header/header.component";
 import HomePage from "../src/pages/homepage/homepage.component";
 import ShopPage from "./pages/shop/shop.component";
+import SingInAndSingOutPage from "./pages/sign-in-and-sign-up/sign-in-and-sign-up.component";
 import "./App.css";
 
 function App() {
@@ -13,6 +14,7 @@ function App() {
       <Switch>
         <Route exact path="/" component={HomePage} />
         <Route path="/shop" component={ShopPage} />
+        <Route path="/signin" component={SingInAndSingOutPage} />
       </Switch>
     </div>
   );
